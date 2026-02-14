884a294605988c59b528b48f37164c2d217ba814
Inaaya14march <m.taz@rediffmail.com>
2026-02-14 17:21:06 +0530

    Remove empty alert in sign invoice button
    Removed empty alert before signing invoice.

--- dsc_signer/public/js/sales_invoice_sign.js
+++ dsc_signer/public/js/sales_invoice_sign.js
@@ -2,7 +2,7 @@ frappe.ui.form.on('Sales Invoice', {
     refresh: function(frm) {
         if (frm.doc.docstatus == 1) {
             frm.add_custom_button(__('Sign Invoice'), function() {
-                alert("");
+               
                 frappe.call({
                     method: 'dsc_signer.api.sign_invoice',
                     args: {
